2a851c9b78cfe7373468d21d0f628bfa26564d1b
Seia-Soto <seia@outlook.kr>
2020-01-22 10:25:59 +0900

    Add heading

---
+++ src/Typography/Heading/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Heading = props => {
+  const {
+    children,
+    as = 'h1',
+    variant = '',
+    ...inheritedProps
+  } = props
+  const Component = as
+
+  return (
+    <Component className={variant} {...inheritedProps}>
+      {children}
+    </Component>
+  )
+}
+
+Heading.propTypes = {
+  children: PropTypes.any,
+  variant: PropTypes.string,
+  as: PropTypes.string.isRequired
+}
+
+export default Heading
